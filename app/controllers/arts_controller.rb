c7e75cf6e7f47c5480776992e0a199222b54a264
watoco015 <fuwari015@gmail.com>
2023-10-11 22:20:52 +0900

    コントローラー

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -2,5 +2,26 @@ class ArtsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
+    @arts = Art.all
   end
+
+  def new
+    @art = Art.new
+  end
+
+  def create
+    @art = Art.create(art_params)
+    if @art.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def art_params
+    params.require(:art).permit(:image, :title, :content, :category_id, :subcategory_id).merge(user_id: current_user.id)
+  end
+
 end
